782e812286150fb496c7be9821e4ae9e89d7e8a1
deboik193 <deboik193@gmail.com>
2020-05-19 07:56:54 -0700

    Initial commit on zero-hunger

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-content>
+      <sideNav />
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import sideNav from "@/components/sideNav";
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: "App",
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    sideNav
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  data: () => ({
+    //
+  })
+};
+</script>
+
+<style lang="css" scoped>
+
 </style>
